fd34009336e295e69e72e74849d2aa020044d879
Alex M <Alexm717@hotmail.com>
2022-09-15 17:13:25 -0700

    story four complete all green

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 
-RSpec.describe("the merchant dashboard") do
-  describe("visit my merchant dashboard") do
+RSpec.describe("1.the merchant dashboard") do
+  describe("1.visit my merchant dashboard") do
     it("I see the name of my merchant") do
       merchant1 = Merchant.create!(      name: "Bob")
       visit("/merchants/#{merchant1.id}/dashboard")
@@ -28,14 +28,6 @@ RSpec.describe("the merchant dashboard") do
       visit("/merchants/#{merchant1.id}/dashboard")
       click_link("Invoices Index")
       expect(current_path).to(eq("/merchants/#{merchant1.id}/invoices"))
-    end
-  end
-
-  describe("visit my merchant dashboard") do
-    it("I see the name of my merchant") do
-      merchant1 = Merchant.create!(      name: "Bob")
-      visit("/merchants/#{merchant1.id}/dashboard")
-      expect(page).to(have_content("#{merchant1.name}"))
     end
   end
 
@@ -67,6 +59,26 @@ RSpec.describe("the merchant dashboard") do
       expect(page).to(have_content("#{item1.name}"))
       expect(page).to(have_content("#{item2.name}"))
       expect(page).to(have_content("#{item3.name}"))
+    end
+  end
+
+  describe("4.And next to each Item I see the id of the invoice that ordered my item") do
+    it("And each invoice id is a link to my merchant's invoice show page") do
+      merchant1 = Merchant.create!(      name: "Bob")
+      customer1 = Customer.create!(      first_name: "cx first name",       last_name: "cx last name")
+      invoice1 = customer1.invoices.create!(      status: 1,       created_at: "2021-09-14 09:00:01")
+      invoice2 = customer1.invoices.create!(      status: 1,       created_at: "2021-09-14 09:00:02")
+      invoice3 = customer1.invoices.create!(      status: 1,       created_at: "2021-09-14 09:00:03")
+      item1 = merchant1.items.create!(      name: "item1",       description: "this is item1 description",       unit_price: 1)
+      item2 = merchant1.items.create!(      name: "item2",       description: "this is item2 description",       unit_price: 2)
+      item3 = merchant1.items.create!(      name: "item3",       description: "this is item3 description",       unit_price: 3)
+      invoice_item1 = InvoiceItem.create!(      item_id: item1.id,       invoice_id: invoice1.id,       unit_price: item1.unit_price,       quantity: 1,       status: 0)
+      invoice_item2 = InvoiceItem.create!(      item_id: item2.id,       invoice_id: invoice2.id,       unit_price: item2.unit_price,       quantity: 2,       status: 0)
+      invoice_item3 = InvoiceItem.create!(      item_id: item3.id,       invoice_id: invoice3.id,       unit_price: item3.unit_price,       quantity: 3,       status: 0)
+      visit("/merchants/#{merchant1.id}/dashboard")
+      save_and_open_page
+      click_link("#{invoice_item1.invoice_id}")
+      expect(current_path).to(eq("/merchants/#{merchant1.id}/invoices/#{invoice_item1.invoice_id}"))
     end
   end
 end
