9d408b11e4f695da23b326f2ec77ecccd50fe6d8
Sid Mann <sjmann2@outlook.com>
2022-09-22 15:10:08 -0500

    Complete setup for bulk discount

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,22 @@
-require "./app/facade/github_facade"
+# require "./app/facade/github_facade"
 
 class ApplicationController < ActionController::Base
-  before_action :user_names, :repo_name, :get_pr
+  # before_action :user_names, :repo_name, :get_pr
 
-  private
+  # private
 
-  def user_names
-    @user_names = GitHubFacade.user_names
-  end
+  # def user_names
+  #   @user_names = GitHubFacade.user_names
+  # end
 
-  def repo_name
-    @repo_name = GitHubFacade.repo_name
-  end
+  # def repo_name
+  #   @repo_name = GitHubFacade.repo_name
+  # end
 
 
-  def get_pr
-    @repo_pr_number = GitHubFacade.pull_requests
-  end
+  # def get_pr
+  #   @repo_pr_number = GitHubFacade.pull_requests
+  # end
 
   # def commits
   #   @commits = GitHubFacade.all_commits
