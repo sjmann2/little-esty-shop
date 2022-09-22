c8862eb2781a37d0d1e6a405537bc9c7d0fc69f6
Sid Mann <sjmann2@outlook.com>
2022-09-22 18:20:08 -0500

    test for merchant bulk discount create

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -1,13 +1,3 @@
-# Merchant Bulk Discounts Index
-
-# As a merchant
-# When I visit my merchant dashboard
-# Then I see a link to view all my discounts
-# When I click this link
-# Then I am taken to my bulk discounts index page
-# Where I see all of my bulk discounts including their
-# percentage discount and quantity thresholds
-# And each bulk discount listed includes a link to its show page
 require 'rails_helper'
 
 RSpec.describe 'the bulk discounts index page' do
@@ -30,7 +20,7 @@ RSpec.describe 'the bulk discounts index page' do
         within "#bulk-discount-#{bulk_discount_1.id}" do
           expect(page).to have_link('View Bulk Discount')
         end
-        save_and_open_page
+        
         within "#bulk-discount-#{bulk_discount_2.id}" do
           expect(page).to have_link('View Bulk Discount')
 
@@ -40,5 +30,54 @@ RSpec.describe 'the bulk discounts index page' do
         expect(current_path).to eq(merchant_bulk_discount_path(merchant_1, bulk_discount_2))
       end
     end
+
+    describe 'I click a link to create a new discount that takes me to a new page' do
+      describe 'When I fill in the form with valid data I am redirected to the bulk discount index' do
+        let!(:merchant_1) {create(:random_merchant)}
+        let!(:bulk_discount_1) {merchant_1.bulk_discounts.create!(percentage_discount: 20, quantity_threshold: 10)}
+        let!(:bulk_discount_2) {merchant_1.bulk_discounts.create!(percentage_discount: 30, quantity_threshold: 15)}
+
+        it 'displays a link to a form to create a new discount' do
+          visit merchant_bulk_discounts_path(merchant_1)
+
+          expect(page).to_not have_content("10% off 5 or more of an item")
+
+          click_link "Create a new discount"
+
+          expect(current_path).to eq(new_merchant_bulk_discount(merchant_1))
+
+          fill_in "Percentage_discount", with: "10"
+          fill_in "Quantity threshold", with: "5"
+          click_on "Submit"
+
+          expect(current_path).to eq(merchant_bulk_discounts_path(merchant_1))
+
+          expect(page).to have_content("10% off 5 or more of an item")
+        end
+
+        it 'Will not redirect if I do not fill in fields with valid data' do
+          visit merchant_bulk_discounts_path(merchant_1)
+          
+          click_link "Create a new discount"
+
+          fill_in "Percentage_discount", with: "10%"
+          fill_in "Quantity threshold", with: "five"
+          click_on "Submit"
+
+          expect(current_path).to eq(merchant_bulk_discounts_path(merchant_1))
+          expect(page).to have_content("Percentage discount is not a number")
+        end
+      end
+    end
   end
 end
+# Merchant Bulk Discount Create
+
+# As a merchant
+# When I visit my bulk discounts index
+# Then I see a link to create a new discount
+# When I click this link
+# Then I am taken to a new page where I see a form to add a new bulk discount
+# When I fill in the form with valid data
+# Then I am redirected back to the bulk discount index
+# And I see my new bulk discount listed
