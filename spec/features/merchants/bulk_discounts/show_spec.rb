670d894dd353105d44b4b66b5e10b09ccf06d862
Sid Mann <sjmann2@outlook.com>
2022-09-23 11:49:47 -0500

    Remove comments

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -54,14 +54,3 @@ RSpec.describe 'the bulk discount show page' do
     end
   end
 end
-# Merchant Bulk Discount Edit
-
-# As a merchant
-# When I visit my bulk discount show page
-# Then I see a link to edit the bulk discount
-# When I click this link
-# Then I am taken to a new page with a form to edit the discount
-# And I see that the discounts current attributes are pre-poluated in the form
-# When I change any/all of the information and click submit
-# Then I am redirected to the bulk discount's show page
-# And I see that the discount's attributes have been updated
