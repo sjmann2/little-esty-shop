e118c0a274da594f25c9c7fee131d387d7014015
Sid Mann <sjmann2@outlook.com>
2022-09-24 15:34:52 -0500

    test/feat discounted revenue for an invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,5 @@
 class Invoice < ApplicationRecord
   enum   status: {cancelled: 0, "in progress" => 1, completed: 2}
-
   has_many :transactions
   has_many :invoice_items
   has_many :items,   through: :invoice_items
@@ -20,5 +19,16 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items
     .sum("invoice_items.quantity * invoice_items.unit_price")
+  end
+
+  def discount
+    invoice_items
+    .joins(item: [{merchant: :bulk_discounts}])
+    .where('quantity >= bulk_discounts.quantity_threshold')
+    .sum('(quantity * invoice_items.unit_price) * (bulk_discounts.percentage_discount / 100.00)')
+  end
+
+  def discounted_revenue
+    total_revenue - discount
   end
 end
