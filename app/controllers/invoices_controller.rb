bd9fa8d3a46bcb8e7e64648047144e5a77d8d832
Sid Mann <sjmann2@outlook.com>
2022-09-25 17:27:13 -0500

    Formatting

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,22 +1,21 @@
 class InvoicesController < ApplicationController
+  def index
+    @invoices = Invoice.all
+    @merchant = Merchant.find(params[:merchant_id])
+  end
 
-    def index
-        @invoices = Invoice.all
-        @merchant = Merchant.find(params[:merchant_id])
-    end
+  def show
+    @invoice = Invoice.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice_items = @invoice.invoice_items
+  end
 
-    def show
-        @invoice = Invoice.find(params[:id])
-        @merchant = Merchant.find(params[:merchant_id])
-        @invoice_items = @invoice.invoice_items
-    end
-
-    def update
-        @invoice = Invoice.find(params[:id])
-        @merchant = Merchant.find(params[:merchant_id])
-        @invoice_item = InvoiceItem.find_by(invoice_id: @invoice.id)
-        @invoice.update(status: params[:status])
-        redirect_to merchant_invoice_path(@merchant, @invoice)
-    end
+  def update
+    @invoice = Invoice.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice_item = InvoiceItem.find_by(invoice_id: @invoice.id)
+    @invoice.update(status: params[:status])
+    redirect_to merchant_invoice_path(@merchant, @invoice)
+  end
 end
 
